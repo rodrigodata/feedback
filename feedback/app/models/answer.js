b13ca89085b0375305bea929e9f7922ba3a1363b
mastercode64 <marcosfujihashi@gmail.com>
2019-09-20 12:45:17 -0300

    Adicionado entidade answer

---
+++ feedback/app/models/answer.js
@@ -0,0 +1,30 @@
+module.exports = {
+    up: (queryInterface, DataTypes) => {
+        return queryInterface.createTable('answer', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            question_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: 'question',
+                referencesKey: 'id'
+            },
+            text: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            username: {
+                allowNull: true,
+                type: DataTypes.STRING,
+            },
+        });
+    },
+
+    down: (queryInterface) => {
+        return queryInterface.dropTable('answer');
+    }
+};
