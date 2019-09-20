be14c1017be370538bfeab093d92d7fc66971141
mastercode64 <marcosfujihashi@gmail.com>
2019-09-20 17:01:21 -0300

    refactoring

--- feedback/app/routes/v1/form/form.js
+++ feedback/app/routes/v1/form/form.js
@@ -4,9 +4,10 @@ const router = require("express").Router();
 /* Import Controllers */
 const FormController = require("../../../controllers/form/form");
 
-router
-  .route("/form/:id")
-  .post(FormController.create)
-  .get(FormController.get)
+router.route("/form")
+    .post(FormController.create);
+
+router.route("/form/:id/answer")
+    .get(FormController.get);
 
 module.exports = router;
